99433f1975f7c0f13de93c7cf2ae9c445dbec5b9
Guillaume B <guillaume@silk-digital.com>
2023-03-20 13:39:37 +0100

    Add of functional examples.

--- src/functionalprogramming/Examples.scala
+++ src/functionalprogramming/Examples.scala
@@ -1,14 +1,25 @@
 package functionalprogramming
 
-
 case object Examples {
+
+  case class SuperHero(name: String, age: Int, power: Option[String])
 
   val oTwo = Option(2)
   val none = Option.empty[Int]
+
+  val listOfInt: Seq[Int] = List(1,2,3,4,5)
+  val listOfInt2: Seq[Int] = List(1,2,3,4,5)
+  val listOfInt3: Seq[Seq[Int]] = List(listOfInt,listOfInt2)
+  val listOfInt4= List(oTwo,none)
 }
 
 Examples.oTwo.map(_ + 1)
 Examples.none.map(_ + 1)
+Examples.listOfInt.map(_ + 1)
+Examples.listOfInt3.map(_.map(_ + 1)).flatten
+Examples.listOfInt3.flatMap(_.map(_ + 1))
+Examples.listOfInt4.map(_.map(_ + 1))
+Examples.listOfInt4.flatMap(_.map(_ + 1))
 
 
 
